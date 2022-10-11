38f4a34933a01a5f4575e50548ccd3bdf747247c
Hiasintus <hiasintusguido@gmail.com>
2022-10-11 20:17:02 +0700

    tugas angular(hiasintus Guido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
